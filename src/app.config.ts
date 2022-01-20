68b69846755426363f5beb20df4e4ee4fc20885d
Mankin <vinkencheng@gmail.com>
2022-01-20 11:12:28 +0800

    feat: 自定义 tabbar

--- src/app.config.ts
+++ src/app.config.ts
@@ -6,6 +6,19 @@ export default {
       pages: ['404'],
     }
   ],
+  tabBar: {
+    custom: true,
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+      },
+      {
+        pagePath: 'pages/user/index',
+        text: '我的',
+      },
+    ],
+  },
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
---
+++ src/custom-tab-bar/index.tsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import Taro from '@tarojs/taro'
+import { View } from '@tarojs/components'
+import Router from '@/utils/router'
+import './index.scss'
+
+interface TabbarItem {
+  pagePath: string
+  text: string
+}
+
+const list: TabbarItem[] = [
+  {
+    pagePath: 'pages/index/index',
+    text: '首页',
+  },
+  {
+    pagePath: 'pages/user/index',
+    text: '我的',
+  },
+]
+
+const CustomTabBar = (): JSX.Element => {
+  const [currentTab, setCurrentTab] = useState('')
+
+  const handleSwtich = (item: any) => {
+    Taro.switchTab({
+      url: `/${item.pagePath}`,
+    })
+  }
+
+  useEffect(() => {
+    // 默认选中当前页面
+    const currentRoute = Router.getCurrentRoute()
+    if (currentRoute) {
+      setCurrentTab(currentRoute)
+    }
+
+    // 监听变化
+    wx.onAppRoute((res: { path: string }) => {
+      if (res.path) {
+        setCurrentTab(res.path)
+      }
+    })
+  }, [])
+
+  return (
+    <View className='tabbar-container'>
+      {list.map(item => {
+        return (
+          <View
+            onClick={() => handleSwtich(item)}
+            key={item.pagePath}
+            className={`tabbar-item ${item.pagePath === currentTab ? 'tabbar-item-selected' : ''
+              }`}
+          >
+            <View className='tabbar-item-text'>{item.text}</View>
+          </View>
+        )
+      })}
+    </View>
+  )
+}
+
+export default CustomTabBar
